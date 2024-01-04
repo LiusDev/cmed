0337430094eeb78eb316a260e3ef721d58658da3
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-01-04 19:33:52 +0700

    feat: contact fnc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,10 @@ import i18n from "@/translation/i18n"
 import "@mantine/core/styles.css"
 import "@/styles/globals.css"
 import "@mantine/carousel/styles.css"
+import "@mantine/notifications/styles.css"
 
 import { MantineProvider, createTheme } from "@mantine/core"
+import { Notifications } from "@mantine/notifications"
 
 const theme = createTheme({})
 
@@ -13,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <I18nextProvider i18n={i18n}>
             <MantineProvider theme={theme}>
+                <Notifications />
                 <Component {...pageProps} />
             </MantineProvider>
         </I18nextProvider>
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,8 +2,84 @@ import { Button, Trans } from "@/components/common"
 import { MainLayout } from "@/components/layout"
 import React from "react"
 import { MdArrowForward } from "react-icons/md"
+import { useForm } from "@mantine/form"
+import { doPost } from "@/utils"
+import { notifications } from "@mantine/notifications"
+import { CreateContactDto } from "@/types"
 
 const Contact = () => {
+    const form = useForm({
+        initialValues: {
+            isPersonal: true,
+            name: "",
+            phone: "",
+            company: "",
+            email: "",
+            content: "",
+        },
+
+        validate: {
+            name: (value) =>
+                value.trim().length > 0 ? null : "Vui lòng nhập tên",
+            phone: (value) =>
+                value.trim().length > 0 ? null : "Vui lòng nhập số điện thoại",
+            company: (value) =>
+                value.trim().length > 0 ? null : "Vui lòng nhập tên công ty",
+            email: (value) =>
+                /^\S+@\S+$/.test(value)
+                    ? null
+                    : "Vui lòng nhập đúng định dạng email",
+            content: (value) =>
+                value.trim().length > 0
+                    ? null
+                    : "Vui lòng nhập nội dung tin nhắn",
+        },
+    })
+
+    const handleChangeSelect = (
+        event: React.ChangeEvent<HTMLSelectElement>
+    ) => {
+        let value = true
+        if (event.target.value === "false") {
+            value = false
+        } else {
+            value = true
+        }
+        form.setFieldValue("isPersonal", value)
+    }
+
+    const handleSubmit = async () => {
+        form.validate()
+
+        if (!form.isValid()) {
+            notifications.show({
+                title: "Không thể gửi tin nhắn",
+                message: Object.values(form.errors)[0],
+                color: "red",
+            })
+            return
+        }
+
+        const { data, error } = await doPost<CreateContactDto>(
+            "/contacts",
+            form.values
+        )
+        if (error) {
+            notifications.show({
+                title: "Không thể gửi tin nhắn",
+                message: "Vui lòng thử lại sau",
+                color: "red",
+            })
+        } else {
+            notifications.show({
+                title: "Gửi tin nhắn thành công",
+                message: "Chúng tôi sẽ liên hệ với bạn sớm nhất có thể",
+                color: "green",
+            })
+            form.reset()
+        }
+    }
+
     return (
         <MainLayout>
             <div className="container m-auto px-4 mt-28">
@@ -14,13 +90,16 @@ const Contact = () => {
                 <h2 className="text-center mb-8 text-2xl">
                     <Trans text="contact.description" />
                 </h2>
-                <form className="grid grid-cols-2 gap-x-16 gap-y-8 w-full max-w-3xl m-auto mb-20">
+                <div className="grid grid-cols-2 gap-x-16 gap-y-8 w-full max-w-3xl m-auto mb-20">
                     <div className="col-span-2 grid grid-cols-2 gap-x-16">
                         <div className="flex flex-col gap-2">
                             <label htmlFor="" className="font-medium">
                                 <Trans text="contact.form.userType.label" />
                             </label>
-                            <select className="border-b border-tertiary outline-none">
+                            <select
+                                className="border-b border-tertiary outline-none pb-1"
+                                onChange={handleChangeSelect}
+                            >
                                 <option value="true">
                                     <Trans text="contact.form.userType.options.personal" />
                                 </option>
@@ -36,7 +115,8 @@ const Contact = () => {
                         </label>
                         <input
                             type="text"
-                            className="border-b border-tertiary outline-none"
+                            className="border-b border-tertiary outline-none pb-1"
+                            {...form.getInputProps("name")}
                         />
                     </div>
                     <div className="flex flex-col gap-2">
@@ -45,7 +125,8 @@ const Contact = () => {
                         </label>
                         <input
                             type="text"
-                            className="border-b border-tertiary outline-none"
+                            className="border-b border-tertiary outline-none pb-1"
+                            {...form.getInputProps("phone")}
                         />
                     </div>
                     <div className="flex flex-col gap-2">
@@ -54,7 +135,8 @@ const Contact = () => {
                         </label>
                         <input
                             type="text"
-                            className="border-b border-tertiary outline-none"
+                            className="border-b border-tertiary outline-none pb-1"
+                            {...form.getInputProps("company")}
                         />
                     </div>
                     <div className="flex flex-col gap-2">
@@ -63,19 +145,27 @@ const Contact = () => {
                         </label>
                         <input
                             type="text"
-                            className="border-b border-tertiary outline-none"
+                            className="border-b border-tertiary outline-none pb-1"
+                            {...form.getInputProps("email")}
                         />
                     </div>
                     <div className="flex flex-col gap-2 col-span-2">
                         <label htmlFor="" className="font-medium">
                             <Trans text="contact.form.message" />
                         </label>
-                        <textarea className="bg-tertiary-light/10 rounded-md outline-none p-6 h-60"></textarea>
+                        <textarea
+                            className="bg-tertiary-light/10 rounded-md outline-none p-6 h-60"
+                            {...form.getInputProps("content")}
+                        ></textarea>
                     </div>
-                    <Button className="col-span-2" variant="outline">
+                    <Button
+                        className="col-span-2"
+                        variant="outline"
+                        onClick={handleSubmit}
+                    >
                         <Trans text="contact.form.send" /> <MdArrowForward />
                     </Button>
-                </form>
+                </div>
             </div>
         </MainLayout>
     )

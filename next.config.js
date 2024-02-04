f6e6895b6dd341c97d2b9c2f12dc6064db04e183
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-02-05 03:38:29 +0700

    feat: pdf viewer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.node/,
+            type: "raw-loader",
+        })
+        config.resolve.alias.canvas = false
+        return config
+    },
 }
 
 module.exports = nextConfig

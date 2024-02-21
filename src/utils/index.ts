0738ced313cef6a11ffb9d25869ef675b4e6fc5b
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-02-21 08:02:05 +0700

    fix: api url

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,9 +5,6 @@ import * as https from "https"
 
 export const instance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
-    httpsAgent: new https.Agent({
-        rejectUnauthorized: false,
-    }),
 })
 
 const fetcher = (url: string) => instance.get(url).then((res) => res.data)

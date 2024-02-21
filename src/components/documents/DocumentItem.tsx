3209cd8be49eb6ae23c626c70730889f93b3cd2b
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-02-21 15:06:50 +0700

    feat: document feature image

--- src/components/documents/DocumentItem.tsx
+++ src/components/documents/DocumentItem.tsx
@@ -1,20 +1,24 @@
-import React from "react";
-import type { Document } from "@/types";
-import Link from "next/link";
+import React from "react"
+import type { Document } from "@/types"
+import Link from "next/link"
 interface ItemProps {
-  document: Document;
+    document: Document
 }
 const DocumentItem = ({ document }: ItemProps) => {
-  return (
-    <div className="p-5 bg-[#f3faff] overflow-hidden">
-      <Link href={`/documents/${document.id}`}>
-        <img src="/home/about.png" className="px-3" alt={document.name} />
-        <h2 className="pt-5 text-base font-medium line-clamp-2 overflow-hidden">
-          {document.name}
-        </h2>
-      </Link>
-    </div>
-  );
-};
+    return (
+        <div className="p-5 bg-[#f3faff] overflow-hidden">
+            <Link href={`/documents/${document.id}`}>
+                <img
+                    src={document.featuredImage || "/home/about.png"}
+                    className="px-3"
+                    alt={document.name}
+                />
+                <h2 className="pt-5 text-base font-medium line-clamp-2 overflow-hidden">
+                    {document.name}
+                </h2>
+            </Link>
+        </div>
+    )
+}
 
-export default DocumentItem;
+export default DocumentItem

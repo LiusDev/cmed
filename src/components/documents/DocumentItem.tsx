5ad9f63961a90a390989f229be95f0f574d9fcc6
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-02-21 15:53:59 +0700

    feat: document loading state

--- src/components/documents/DocumentItem.tsx
+++ src/components/documents/DocumentItem.tsx
@@ -1,19 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 import type { Document } from "@/types"
 import Link from "next/link"
+import { LoadingOverlay } from "@mantine/core"
 interface ItemProps {
     document: Document
 }
 const DocumentItem = ({ document }: ItemProps) => {
+    const [loading, setLoading] = useState(false)
     return (
-        <div className="p-5 bg-[#f3faff] overflow-hidden">
-            <Link href={`/documents/${document.id}`}>
+        <div className="p-5 bg-[#f3faff] overflow-hidden relative">
+            <Link
+                href={`/documents/${document.id}`}
+                onClick={() => setLoading(true)}
+            >
+                <LoadingOverlay
+                    visible={loading}
+                    zIndex={1000}
+                    overlayProps={{ radius: "sm", blur: 2 }}
+                />
                 <img
                     src={document.featuredImage || "/home/about.png"}
                     className="px-3"
                     alt={document.name}
                 />
-                <h2 className="pt-5 text-base font-medium line-clamp-2 overflow-hidden">
+                <h2 className="px-3 pt-5 text-base font-medium line-clamp-2 overflow-hidden">
                     {document.name}
                 </h2>
             </Link>
--- src/pages/documents/[id].tsx
+++ src/pages/documents/[id].tsx
@@ -1,90 +1,92 @@
-import { BreadCr, Trans } from "@/components/common";
-import { MainLayout } from "@/components/layout";
-import { Document } from "@/types";
-import { formatDate, instance } from "@/utils";
-import { GetServerSidePropsContext } from "next";
-import parse from "html-react-parser";
-import React from "react";
-import { useTranslation } from "react-i18next";
-import { DocumentItem } from "@/components/documents";
-import { Grid } from "@mantine/core";
-import PdfViewer from "@/components/pdfViewer/PdfViewer";
+import { BreadCr, Trans } from "@/components/common"
+import { MainLayout } from "@/components/layout"
+import { Document } from "@/types"
+import { formatDate, instance } from "@/utils"
+import { GetServerSidePropsContext } from "next"
+import parse from "html-react-parser"
+import React from "react"
+import { useTranslation } from "react-i18next"
+import { DocumentItem } from "@/components/documents"
+import { Grid } from "@mantine/core"
+import PdfViewer from "@/components/pdfViewer/PdfViewer"
 
 interface NewsDetailProps {
-  documents: Document;
-  relatedDocuments: Document[];
+    document: Document
+    relatedDocuments: Document[]
 }
 
-const DocumentsDetail = ({ documents, relatedDocuments }: NewsDetailProps) => {
-  const { t } = useTranslation();
+const DocumentsDetail = ({ document, relatedDocuments }: NewsDetailProps) => {
+    const { t } = useTranslation()
 
-  const breadCrumbsItems = [
-    {
-      name: t("documents.title"),
-      link: "/documents",
-    },
-    {
-      name: documents.category.name,
-      link: `/documents?c=${documents.category.id}`,
-    },
-  ];
+    const breadCrumbsItems = [
+        {
+            name: t("documents.title"),
+            link: "/documents",
+        },
+        {
+            name: document.category.name,
+            link: `/documents?c=${document.category.id}`,
+        },
+    ]
 
-  return (
-    <MainLayout>
-      <div className="container m-auto px-4 mt-28">
-        <BreadCr items={breadCrumbsItems} />
-        <h1 className="text-2xl md:text-4xl font-bold uppercase mb-4">
-          {documents.name}
-        </h1>
-        <p className="text-sm mb-2">{formatDate(documents.createdAt, " - ")}</p>
-        <div className="pb-10 border-b border-tertiary/20">
-          {parse(documents.description)}
-        </div>
+    return (
+        <MainLayout>
+            <div className="container m-auto px-4 mt-28">
+                <BreadCr items={breadCrumbsItems} />
+                <h1 className="text-2xl md:text-4xl font-bold uppercase mb-4">
+                    {document.name}
+                </h1>
+                <p className="text-sm mb-2">
+                    {formatDate(document.createdAt, " - ")}
+                </p>
+                <div className="pb-10 border-b border-tertiary/20">
+                    {parse(document.description)}
+                </div>
 
-        <PdfViewer url={documents.document} />
+                <PdfViewer url={document.document} />
 
-        <h2 className="text-xl md:text-3xl uppercase my-10">
-          <Trans text="documents.detail.related" />
-        </h2>
-        <Grid className="w-full pb-20">
-          {relatedDocuments.length > 0 &&
-            relatedDocuments.map((item) => (
-              <Grid.Col span={3}>
-                <DocumentItem key={item.id} document={item} />
-              </Grid.Col>
-            ))}
-        </Grid>
-      </div>
-    </MainLayout>
-  );
-};
+                <h2 className="text-xl md:text-3xl uppercase my-10">
+                    <Trans text="documents.detail.related" />
+                </h2>
+                <Grid className="w-full pb-20">
+                    {relatedDocuments.length > 0 &&
+                        relatedDocuments.map((item) => (
+                            <Grid.Col span={3}>
+                                <DocumentItem key={item.id} document={item} />
+                            </Grid.Col>
+                        ))}
+                </Grid>
+            </div>
+        </MainLayout>
+    )
+}
 
 export const getServerSideProps = async (
-  context: GetServerSidePropsContext
+    context: GetServerSidePropsContext
 ) => {
-  if (!context.params || typeof context.params.id !== "string") {
+    if (!context.params || typeof context.params.id !== "string") {
+        return {
+            notFound: true,
+        }
+    }
+    const { id } = context.params
+    const { data: document }: { data: Document } = await instance.get(
+        `/documents/${id}`
+    )
+    const { data }: { data: Document[] } = await instance.get(
+        `/documents?c=${document.category.id}&perPage=4`
+    )
+
+    const relatedDocuments = data
+        .filter((item) => item.id !== document.id)
+        .slice(0, 3)
+
     return {
-      notFound: true,
-    };
-  }
-  const { id } = context.params;
-  const { data: documents }: { data: Document } = await instance.get(
-    `/documents/${id}`
-  );
-  const { data }: { data: Document[] } = await instance.get(
-    `/documents?c=${documents.category.id}&perPage=4`
-  );
+        props: {
+            document,
+            relatedDocuments,
+        },
+    }
+}
 
-  const relatedDocuments = data
-    .filter((item) => item.id !== documents.id)
-    .slice(0, 3);
-
-  return {
-    props: {
-      documents,
-      relatedDocuments,
-    },
-  };
-};
-
-export default DocumentsDetail;
+export default DocumentsDetail

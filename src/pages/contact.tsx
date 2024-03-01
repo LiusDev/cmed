6fead36b079e2df37440ad07862a94dcc221a5ab
Tran Van Khuyen <khuyentvhe187268@fpt.edu.vn>
2024-03-01 13:47:32 +0700

    add: contact/map

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -10,7 +10,7 @@ const ContactPage = () => {
                 <h1 className="pb-8 mb-20 text-center border-b border-tertiary/50 text-2xl md:text-4xl uppercase">
                     <Trans text="contact.title" />
                 </h1>
-                <img src="/contact/map.png" alt="Map" className="my-20" />
+                <iframe className="w-full h-[600px] mb-10 px-10" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.6774841946835!2d105.80112081143572!3d21.005561480557176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ad964441328b%3A0x55d99271bdda1425!2sDiamond%20Flower%20Tower!5e0!3m2!1svi!2s!4v1709275361684!5m2!1svi!2s" width="600" height="450" style={{ border: 0 }} allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade" ></iframe>
                 <h2 className="text-center mb-8 text-lg md:text-2xl">
                     <Trans text="contact.description" />
                 </h2>

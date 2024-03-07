975fedfc492bb662b926f67b598773364e19c42e
Tran Van Khuyen <khuyentvhe187268@fpt.edu.vn>
2024-03-07 09:37:11 +0700

    fix bug

--- src/components/about/Testimonials.tsx
+++ src/components/about/Testimonials.tsx
@@ -20,32 +20,29 @@ const Testimonials = () => {
   data.length <= 4 && (data = [...data, ...data]);
 
   return (
-    <div className=" bg-[url('/about/testimonials/bg.png')] bg-cover bg-no-repeat flex justify-center items-center mb-20 h-full">
-      <div className="h-full w-full flex justify-center bg-[#7493BC]/60 ">
-        <Carousel
-          classNames={classes}
-          withIndicators
-          height="100%"
-          // slideSize={{ base: "100%", sm: "50%", md: "33.333333%" }}
-          slideGap={{ base: 0, sm: "md" }}
-          loop
-        >
-          {data.map((item, index) => (
-            <Carousel.Slide key={index}>
-              <div className="mx-auto w-full md:w-3/5 text-center h-full md:py-20 text-secondary flex flex-col items-center justify-center">
-                <img
-                  src="/about/testimonials/Vector.png"
-                  alt="quotation"
-                  className="w-10 h-10 md:w-12 md:h-12 my-10"
-                />
-                <p className="text-lg md:text-xl px-10">{item.content}</p>
-                <div className="border md:w-60 md:my-8"></div>
-                <div className="text-2xl py-10">{item.author}</div>
-              </div>
-            </Carousel.Slide>
-          ))}
-        </Carousel>
-      </div>
+    <div className=" bg-[url('/about/testimonials/bg.png')] bg-cover bg-no-repeat flex justify-center items-center mb-20">
+      <Carousel
+        classNames={classes}
+        withIndicators
+        height="100%"
+        loop
+        className="bg-[#7493BC]/60"
+      >
+        {data.map((item, index) => (
+          <Carousel.Slide key={index}>
+            <div className="mx-auto md:w-3/5  text-center md:py-20 text-secondary flex flex-col items-center justify-center">
+              <img
+                src="/about/testimonials/Vector.png"
+                alt="quotation"
+                className="w-10 h-10 md:w-12 md:h-12 my-10"
+              />
+              <p className="text-lg md:text-xl">{item.content}</p>
+              <div className="border md:w-60 md:my-8"></div>
+              <div className="text-2xl py-10">{item.author}</div>
+            </div>
+          </Carousel.Slide>
+        ))}
+      </Carousel>
     </div>
   );
 };

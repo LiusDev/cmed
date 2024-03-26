f42307f5374d09cd4da019d0538810d7cef9da08
rezarria <votanamcb@outlook.com>
2024-03-26 08:57:44 +0700

    fix: hidden description in service.

--- src/components/service/Banner.tsx
+++ src/components/service/Banner.tsx
@@ -1,16 +1,26 @@
 import { Button, Trans } from "../common";
 
-const Banner = () => {
+
+interface BannerProps {
+  title?: string
+  description?: string
+}
+
+const Banner = (props: BannerProps) => {
   return (
     <section className="mt-16">
       <div className="bg-[url('/home/home-banner.png')] bg-no-repeat bg-center bg-cover grid grid-cols-12">
         <div className="col-span-12 md:col-span-6 lg:col-span-4 bg-primary/50 flex items-center justify-center py-40 px-4 lg:px-20 transition-all">
           <div className="text-secondary">
             <h1 className="text-7xl font-bold mb-12">
-              <Trans text="services.banner.title" />
+              {
+                props.title ?? <Trans text="services.banner.title" />
+              }
             </h1>
             <p className="mb-12 text-lg font-light">
-              <Trans text="services.banner.description" />
+              {
+                props.description ?? <Trans text="services.banner.description" />
+              }
             </p>
             <div className="flex gap-4">
               <Button

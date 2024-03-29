d8a4e0841e9b9c1355835ed76828af5e71fe1bc9
rezarria <votanamcb@outlook.com>
2024-03-29 10:04:27 +0700

    Refactor About component layout

--- src/components/homepage/About.tsx
+++ src/components/homepage/About.tsx
@@ -8,21 +8,21 @@ interface AboutProps {
 const About = ({ className }: AboutProps) => {
   return (
     <section className={twMerge(`my-20 ${className}`)}>
-      <div className="relative grid grid-cols-12">
-        <div className="flex flex-col xl:col-span-6 lg:col-span-8 md:col-span-10 col-span-12 z-10 min-h-[400px]">
-          <div className="md:h-1/2 relative px-5">
-            <h2 className="md:absolute xl:w-1/2 lg:w-2/3 right-12 top-10 capitalize text-4xl md:text-5xl lg:text-6xl leading-normal md:border-b-2 border-tertiary pb-4 font-semibold text-center md:text-left">
+      <div className="relative lg:grid lg:grid-cols-12">
+        <div className="flex flex-col xl:col-span-6 lg:col-span-8 z-10 lg:min-h-[400px]">
+          <div className="lg:h-1/2 relative px-5">
+            <h2 className="lg:absolute xl:w-1/2 lg:w-2/3 right-12 top-10 capitalize text-4xl lg:text-5xl leading-normal lg:border-b-2 border-tertiary pb-4 font-semibold text-center lg:text-left">
               <Trans text="home.about.title" />
             </h2>
           </div>
-          <div className="bg-secondary-dark md:h-3/5 flex justify-center items-center p-10 lg:p-20 lg:text-base xl:text-xl text-sm ">
+          <div className="bg-secondary-dark lg:h-3/5 flex justify-center items-center p-10 lg:p-20 lg:text-base xl:text-xl text-sm ">
             <Trans text="home.about.description" />
           </div>
         </div>
-        <div className="hidden lg:block w-full xl:col-span-4 lg:col-span-3 xl:col-start-7 lg:col-start-9 z-10 min-h-[600px]">
-          <img src="/home/about.webp" alt="home/about image" className="h-full object-cover object-center" />
+        <div className="lg:block w-full xl:col-span-4 lg:col-span-3 xl:col-start-7 lg:col-start-9 z-10 min-h-[600px]">
+          <img src="/home/about.webp" alt="home/about image" className="w-full lg:w-auto lg:h-full object-cover object-center" />
         </div>
-        <div className="absolute hidden md:block bg-primary w-3/5 h-3/5 z-5 right-0"></div>
+        <div className="absolute hidden lg:block bg-primary w-3/5 h-3/5 z-5 right-0"></div>
       </div>
     </section>
   );

d05eb48580bb9e4ec8f92452e4650c4a13c31335
rezarria <votanamcb@outlook.com>
2024-04-01 08:39:59 +0700

    Remove service detail section from [id].tsx

--- src/pages/service/[id].tsx
+++ src/pages/service/[id].tsx
@@ -21,25 +21,6 @@ const ServiceDetail = (props: {
         <MainLayout>
             <Banner title={props.service.name} />
             <Services services={props.services} />
-            <div className="flex flex-col lg:flex-row xl:px-60 mt-10 items-center">
-                {props.service && (
-                    <>
-                        <div className="lg:w-1/2 px-10 pb-20 lg:pb-0  mt-[65px] ">
-                            <div className="relative">
-                                <img src={props.service.featuredImage} alt="image" className="lg:w-[80%] lg:h-[80%]" />
-                                <img src={props.service.featuredImage} alt="image" className="hidden lg:block absolute w-[80%] h-auto -top-[45px] -left-[45px] -z-10" />
-                            </div>
-                        </div>
-
-                        <div className="flex flex-col justify-center lg:w-1/2 px-10 space-y-10">
-                            <h2 className="text-3xl font-bold text-primary">
-                                <Trans text="services.detail.services.title" />
-                            </h2>
-                            <div className="text-justify">{parse(props.service.content)}</div>
-                        </div>
-                    </>
-                )}
-            </div>
             <WhyUs />
 
         </MainLayout>

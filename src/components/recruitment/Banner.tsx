fced94345c1c82a397519804a58e57fefdecbdfc
rezarria <votanamcb@outlook.com>
2024-04-10 19:08:26 +0700

    Refactor Banner component to use useCallback for event handlers and update search functionality

--- src/components/recruitment/Banner.tsx
+++ src/components/recruitment/Banner.tsx
@@ -1,14 +1,18 @@
 import { useRouter } from "next/router"
 import { Button, Trans } from "../common"
-import { useState } from "react"
+import { useCallback, useState, type ChangeEventHandler } from "react"
 
 const Banner = () => {
     const router = useRouter()
     const [keyword, setKeyword] = useState("")
 
-    const handleSearch = () => {
+    const handleSearchButton = useCallback(() => {
         router.push(`/project?keyword=${keyword}`)
-    }
+    }, [keyword, router])
+
+    const handleSearchInput = useCallback<ChangeEventHandler<HTMLInputElement>>(e => { setKeyword(e.target.value) }, [])
+
+
     return (
         <section className="mt-16">
             <div className="bg-[url('/home/home-banner.webp')] bg-no-repeat bg-center bg-cover aspect-4/3 md:aspect-21/9 lg:aspect-4/1 flex justify-center items-end">
@@ -18,9 +22,9 @@ const Banner = () => {
                         className="w-full max-w-[800px] h-12 bg-white px-8 outline-none"
                         placeholder="Tìm kiếm vị trí tuyển dụng"
                         value={keyword}
-                        onChange={(e) => setKeyword(e.target.value)}
+                        onChange={handleSearchInput}
                     />
-                    <Button className="h-12" onClick={handleSearch}>
+                    <Button className="h-12" onClick={handleSearchButton}>
                         <Trans text="common.search" />
                     </Button>
                 </div>
